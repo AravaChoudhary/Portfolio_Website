89265abd848aacec45a8e8577aa9a180a04a933e
Arava Choudhary <aravachoudhary11@gmail.com>
2024-08-18 00:07:54 +0530

    home.html commit

--- ELF/ELF/settings.py
+++ ELF/ELF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0&96kj52q(i0faz70y!uu+2(wp99j=k257=h7*u=f9w+$u(e)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 #'aranika.me','localhost','192.168.248.57', '192.168.1.5'
 
 
@@ -86,11 +86,13 @@ WSGI_APPLICATION = 'ELF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'asmfYVw1Qpg2',
+        'HOST': 'ep-calm-lake-a4b4ai6g-pooler.us-east-1.aws.neon.tech', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -127,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL= '/static/'
-STATIC_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_URL = '/media/'
