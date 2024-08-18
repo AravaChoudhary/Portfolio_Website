5c5ea0061eff1a2f005b60f62c25fe36968b930b
Arava Choudhary <aravachoudhary11@gmail.com>
2024-08-18 11:14:26 +0530

    DataBase Configurations

--- ELF/settings.py
+++ ELF/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,14 +85,18 @@ WSGI_APPLICATION = 'ELF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'verceldb',
+#         'USER': 'default',
+#         'PASSWORD': 'asmfYVw1Qpg2',
+#         'HOST': 'ep-calm-lake-a4b4ai6g-pooler.us-east-1.aws.neon.tech', 
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'verceldb',
-        'USER': 'default',
-        'PASSWORD': 'asmfYVw1Qpg2',
-        'HOST': 'ep-calm-lake-a4b4ai6g-pooler.us-east-1.aws.neon.tech', 
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
